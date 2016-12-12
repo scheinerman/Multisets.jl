9f158b44a7088c282a6c2127a476879c339e788a
Ed Scheinerman <ers@jhu.edu>
2016-12-12 12:18:35 -0500

    Starting

---
+++ src/Multisets.jl
@@ -0,0 +1,22 @@
+module Multisets
+
+export Multiset
+
+"""
+A `Multiset` is an unordered collection of things with repetition permitted.
+A new `Multiset` container is created with `Multiset{T}()` where `T` is
+type of the objects held in the multiset. If `T` is omitted, it defaults
+to `Any`.
+"""
+type Multiset{T}
+  data::Dict{T,Int}
+  function Multiset()
+    d = Dict{T,Int}()
+    new(d)
+  end
+end
+
+Multiset() = Multiset{Any}()
+
+
+end #end of Module
